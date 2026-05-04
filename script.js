87914e7e04dcff873553085a54551f8052ef673e
JirehWang <jireh199301@gmail.com>
2026-05-04 22:13:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -129,7 +129,7 @@ let currentEventData = [];
 // ⚙️ 如果沒有新版 config.js，在這裡直接填入你的部署網址與 Token
 const _FALLBACK_GAS_URL   = "https://script.google.com/macros/s/AKfycbx4268IkgwQm2Es0gjDHLU_U9nKJrRMR1-xzbbtuaq08lePLgAQ2wnDRrCeHdy9jNhh/exec";
 const _FALLBACK_GAS_TOKEN = "ChurchApp-2026";
-const _API_TIMEOUT_MS     = 30000;
+const _API_TIMEOUT_MS     = 120000;
 
 function _getGasUrl() {
   return (window.CHURCH_CONFIG && window.CHURCH_CONFIG.GAS_DEPLOY_URL) ||
